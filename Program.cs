54f9cd18232c9b90926923923ddc28febe3cae60
Daixewe <fuentesvd56@gmail.com>
2022-07-06 15:18:44 -0600

    Arrays loops in c#

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Array loop");
+
+const int size = 5;
+int[] numbers = new int [size];
+
+for (int i = 0;i < size; i++)
+{
+ Console.Write("Ingrese un número: ");
+ string input = Console.ReadLine() ?? "0";
+ numbers[i] = Int32.Parse(input);
+}
+
+
+ int total = 0;
+ for (int i=0; i<size; i++)
+ {
+ total = total + numbers[i];
+
+ }
+
+ for (int i=0; i< size; i++)
+ {
+
+     Console.WriteLine(numbers[i]);
+
+ }
+ Console.WriteLine("Total: " + total);
